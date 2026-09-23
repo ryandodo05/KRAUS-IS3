b1c6ed9819c113b01d1090be02601f35ca904908
Ryan <ryan.kraus.pro@gmail.com>
2026-09-23 09:52:54 +0200

    Creation page principale - Menu de naviagation pour les seances de cours

--- KRAUS-IS3/MainWindow.xaml.cs
+++ KRAUS-IS3/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace KRAUS_IS3
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSeance2_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Seance2());
+        }
+
+        private void btnSeance3_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Seance3());
+        }
+
+        private void btnSeance4_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Seance4());
+        }
+
+        private void btnSeance5_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Seance5());
+        }
+
+        private void btnSeance6_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Seance6());
+        }
+
+        private void btnSeance7_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Seance7());
+        }
+
+        private void btnSeance8_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Seance8());
         }
     }
 }
